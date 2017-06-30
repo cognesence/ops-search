4202bdc88958be9fc851317982f1d0a4a23be2ff
Saul Johnson <saul.a.johnson@gmail.com>
2017-06-30 17:04:19 +0100

    Load libraries by default on REPL

--- project.clj
+++ project.clj
@@ -4,4 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-               [org.clojars.cognesence/matcher "1.0.1"]])
+               [org.clojars.cognesence/matcher "1.0.1"]]
+  :repl-options {:init [(use 'org.clojars.cognesence.matcher.core)
+                        (use 'org.clojars.cognesence.ops-search.core)]})
